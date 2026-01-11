5d95276bb8bda12cb3a0338ee25daa93d3923ed6
Volodymyr <salashchenkovladymyr@gmail.com>
2026-01-11 14:38:39 +0200

    created vite + structure of project

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import { StrictMode } from "react";
+import ReactDom from "react-dom/client";
+import "./index.css";
+import App from "./components/App/App";
+
+ReactDom.createRoot(document.getElementById("root") as HTMLElement).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
