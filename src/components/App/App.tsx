0bd38aaf663354f219e3b29f319c2f208a7ec80c
Volodymyr <salashchenkovladymyr@gmail.com>
2026-01-11 19:09:36 +0200

    add section VoteOptions

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,28 @@
-import CafeInfo from "../CafeInfo/CafeInfo";
 import css from "./App.module.css";
+import CafeInfo from "../CafeInfo/CafeInfo";
+import { useState } from "react";
+import { Votes, VoteType } from "../../types/votes";
+import VoteOptions from "../VoteOptions/VoteOptions";
 
 export default function App() {
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+  const handleVote = (key: keyof Votes) => {
+    setVotes({
+      ...votes,
+      [key]: votes[key] + 1,
+    });
+  };
+  const resetVotes = () => {
+    setVotes({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
   return (
     <div className={css.app}>
       <div className={css.container}>
@@ -10,6 +31,7 @@ export default function App() {
           description="Please rate our service by selecting one of the options below."
         />
       </div>
+      <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={true} />
     </div>
   );
 }
