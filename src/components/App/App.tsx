e29cec16df9d43c5aa54089a3d1d4026df14b46b
Volodymyr <salashchenkovladymyr@gmail.com>
2026-01-11 17:07:44 +0200

    add section CafeInfo

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,3 +1,15 @@
+import CafeInfo from "../CafeInfo/CafeInfo";
+import css from "./App.module.css";
+
 export default function App() {
-  return <></>;
+  return (
+    <div className={css.app}>
+      <div className={css.container}>
+        <CafeInfo
+          title="Sip Happens Café"
+          description="Please rate our service by selecting one of the options below."
+        />
+      </div>
+    </div>
+  );
 }
